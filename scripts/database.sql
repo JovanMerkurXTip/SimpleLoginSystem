d7feb767aa4f0859ff73598fd79dc263f6f7d0aa
JovanDozic <jovandozic@gmail.com>
2024-06-28 13:07:32 +0200

    fix(sql): update database script

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,4 +1,5 @@
--- --------------------------------------------------------
+-- INFORMATION ---------------------------------------------------
+
 -- Database info:
 --      Server version: 10.4.28 MariaDB
 --      Protocol version: 10
@@ -8,41 +9,73 @@
 -- PHPMyAdmin info:
 --      Version information: 5.2.1
 --      PHP 8.2.4
--- --------------------------------------------------------
 
--- TODO: FIX WITH NEW MODEL
-
--- ! DO NOT USE THIS SCRIPT
+-- DATABASE ------------------------------------------------------
 
 CREATE DATABASE IF NOT EXISTS `simplelogin`;
 
-DROP TABLE IF EXISTS password_reset_tokens;
+-- TABLES --------------------------------------------------------
+
 DROP TABLE IF EXISTS login_otp_codes;
+DROP TABLE IF EXISTS password_reset_tokens;
+DROP TABLE IF EXISTS registration_verification_tokens;
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    salt VARCHAR(255) NOT NULL,
-    remember_token VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE `login_otp_codes` (
+  `user_id` int(11) NOT NULL,
+  `otp` varchar(6) NOT NULL,
+  `expiration` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_croatian_ci;
 
-CREATE TABLE login_otp_codes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    otp VARCHAR(6) NOT NULL,
-    expiration DATETIME NOT NULL,
-    UNIQUE KEY user_otp (user_id, otp),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+CREATE TABLE `password_reset_tokens` (
+  `user_id` int(11) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `expiration` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_croatian_ci;
 
-CREATE TABLE password_reset_tokens (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    token VARCHAR(64) NOT NULL,
-    expiration DATETIME NOT NULL,
-    UNIQUE KEY user_token (user_id, token),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+CREATE TABLE `registration_verification_tokens` (
+  `user_id` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `expiration` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_croatian_ci;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL,
+  `remember_token` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_verified` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_croatian_ci;
+
+-- INDEXES -------------------------------------------------------
+
+ALTER TABLE `login_otp_codes`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_otp` (`user_id`,`otp`),
+  ADD UNIQUE KEY `user_id_2` (`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_token` (`user_id`,`token`);
+
+ALTER TABLE `registration_verification_tokens`
+  ADD PRIMARY KEY (`user_id`);
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+-- CONSTRAINTS ---------------------------------------------------
+
+ALTER TABLE `login_otp_codes`
+  ADD CONSTRAINT `login_otp_codes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `password_reset_tokens`
+  ADD CONSTRAINT `password_reset_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+-- ---------------------------------------------------------------
+
+COMMIT;
